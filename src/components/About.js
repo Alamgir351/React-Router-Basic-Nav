be7e59047f757437f9f2d4d3452f60f2b7138ea3
Alamgir <61465527+Alamgir351@users.noreply.github.com>
2020-03-11 13:37:03 -0400

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React from "react";
 
-const About = () => <h1>About</h1>;
-
+const About = () => (
+  <div>
+    {" "}
+    <h1>About</h1> <h2> welcome to About Section</h2>{" "}
+  </div>
+);
 export default About;
